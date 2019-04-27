5aec7290e30d149e947e1be01d2e642fc6642bc1
Marcoswinrar <marcoswinrar@gmail.com>
2019-04-27 11:58:12 -0300

    Updating todo entitie.

--- server/src/main/java/com/supero/todo/entities/TodoItem.java
+++ server/src/main/java/com/supero/todo/entities/TodoItem.java
@@ -1,17 +1,46 @@
 package com.supero.todo.entities;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 @Entity
 public class TodoItem {
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@NotBlank
+	@Column
+	private String title;
+	
+	@Column
 	private String description;
+	
+	@Column
+	private boolean isFinished;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@CreatedDate
+	@Column(nullable = false, updatable = false)
+	private Date creationDate;
+	
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@LastModifiedDate
+	@Column(nullable = false)
+	private Date modifiedDate;
 
 	public Long getId() {
 		return id;
@@ -28,5 +57,36 @@ public class TodoItem {
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public boolean isFinished() {
+		return isFinished;
+	}
+
+	public void setFinished(boolean isFinished) {
+		this.isFinished = isFinished;
+	}
+
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public Date getModifiedDate() {
+		return modifiedDate;
+	}
+
+	public void setModifiedDate(Date modifiedDate) {
+		this.modifiedDate = modifiedDate;
+	}
 }
